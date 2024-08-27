073a725fb325f5fff9d55e4040714e4c4ed208db
김나영 <kny200208@naver.com>
2024-08-27 21:32:41 +0900

    feat : class,reservation,review 테이블 생성

--- Team1/Team1/settings.py
+++ Team1/Team1/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myPage',
+    'detailPage',
+    'mainPage',
+    'signInUp',
+    'Team1'
 ]
 
 MIDDLEWARE = [
